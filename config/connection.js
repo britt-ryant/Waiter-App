b26dd4d811f23b9037132f8be5ffd53c078239b7
CRHarding <casey.r.harding@gmail.com>
2018-03-15 12:00:03 -0400

    Adding smart logging to see our database queries

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,11 @@
 //Require pg-promise and initiate it
+const options = {
+  query: (e) => {
+    console.log(e.query);
+  },
+};
 
-const pgp = require(`pg-promise`)();
-const dbConfig = require(`../config/dbConfig`)
+const pgp = require(`pg-promise`)(options);
+const dbConfig = require(`../config/dbConfig`);
 
 module.exports = pgp(dbConfig);
