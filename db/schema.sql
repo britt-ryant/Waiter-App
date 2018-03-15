406c2cfb8f5ff36f4542ecd1945a8e58a8461322
CRHarding <casey.r.harding@gmail.com>
2018-03-14 20:43:29 -0400

    Adding basic schema / seed information

---
+++ db/schema.sql
@@ -0,0 +1,25 @@
+\c menu_deb;
+
+DROP TABLE IF EXISTS menu_item
+DROP TABLE IF EXISTS users
+DROP TABLE IF EXISTS order
+
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  user_name VARCHAR(255),
+  user_password VARCHAR(255)
+)
+
+CREATE TABLE menu_item (
+  id SERIAL PRIMARY KEY,
+  cost integer default 0,
+  course integer default 0,
+  name VARCHAR(255),
+  description VARCHAR(255),
+  allergy integer default 0 REFERENCES allergy(id) ON UPDATE CASCADE ON DELETE CASCADE,
+)
+
+CREATE TABLE allergy (
+  id SERIAL PRIMARY KEY,
+  description VARCHAR(255),
+)
---
+++ db/seeds.sql
@@ -0,0 +1,19 @@
+INSERT INTO users (user_name, user_password) VALUES
+  (
+    'casey',
+    'casey'
+  )
+
+  INSERT INTO menu_item (cost, course, name, description, allergy) VALUES
+  (
+    15,
+    1,
+    'House Salad',
+    'Mix greens with balsamic vinaigrette',
+    0
+  )
+
+  INSERT INTO allergy(description) VALUES
+  (
+    'No allergies'
+  )
